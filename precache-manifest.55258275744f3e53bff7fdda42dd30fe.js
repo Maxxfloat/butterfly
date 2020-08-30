93ac8c7ea556fc1ba4521915848ebb6d367bd886
Maxxfloat <tchandnofame@gamil.com>
2020-08-30 11:44:08 +0430

    Updates

---
+++ precache-manifest.55258275744f3e53bff7fdda42dd30fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cd24d14233b2f4e885867d316f512c",
+    "url": "/butterfly/index.html"
+  },
+  {
+    "revision": "679c81f57251f6774dd5",
+    "url": "/butterfly/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8a8ca828acce03b5d656",
+    "url": "/butterfly/static/css/main.e212f064.chunk.css"
+  },
+  {
+    "revision": "679c81f57251f6774dd5",
+    "url": "/butterfly/static/js/2.2bb6517b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/butterfly/static/js/2.2bb6517b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8ca828acce03b5d656",
+    "url": "/butterfly/static/js/main.1a67ad40.chunk.js"
+  },
+  {
+    "revision": "8a099a0271385bad3e5b",
+    "url": "/butterfly/static/js/runtime-main.e76fee25.js"
+  }
+]);
